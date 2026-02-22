3878d386c357188045e5b092d62af871544568d6
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2026-02-22 11:51:52 +0700

    idk but it works now

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Auth\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
